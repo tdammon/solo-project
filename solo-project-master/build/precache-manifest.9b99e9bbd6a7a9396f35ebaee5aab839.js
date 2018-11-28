236982f937afc030789a37c651c1f31a3d428bec
The Goose <trevordammon@gmail.com>
2018-11-28 15:28:38 -0600

    pushing to heroku

---
+++ solo-project-master/build/precache-manifest.9b99e9bbd6a7a9396f35ebaee5aab839.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "62f91265b5db80f4610a",
+    "url": "/static/js/main.62f91265.chunk.js"
+  },
+  {
+    "revision": "789916fc110385ac0338",
+    "url": "/static/js/1.789916fc.chunk.js"
+  },
+  {
+    "revision": "62f91265b5db80f4610a",
+    "url": "/static/css/main.813cdc69.chunk.css"
+  },
+  {
+    "revision": "ddd769cea7b687ec1cdc5f502ea9ef5b",
+    "url": "/index.html"
+  }
+];
